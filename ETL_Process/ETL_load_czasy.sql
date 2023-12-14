93f4fbced63e1ebf670530a94ce3fa1c92d3944e
Justyna Dąbrowska <tyska.dabrowska@gmail.com>
2023-12-14 22:57:52 +0100

    load czas

---
+++ ETL_Process/ETL_load_czasy.sql
@@ -0,0 +1,27 @@
+USE FaktyWypozyczenia
+go
+
+insert into dbo.DimCzas("Godzina", "Pora_Dnia") values (0, 'Noc');
+insert into dbo.DimCzas("Godzina", "Pora_Dnia") values (1, 'Noc');
+insert into dbo.DimCzas("Godzina", "Pora_Dnia") values (2, 'Noc');
+insert into dbo.DimCzas("Godzina", "Pora_Dnia") values (3, 'Noc');
+insert into dbo.DimCzas("Godzina", "Pora_Dnia") values (4, 'Noc');
+insert into dbo.DimCzas("Godzina", "Pora_Dnia") values (5, 'Rano');
+insert into dbo.DimCzas("Godzina", "Pora_Dnia") values (6, 'Rano');
+insert into dbo.DimCzas("Godzina", "Pora_Dnia") values (7, 'Rano');
+insert into dbo.DimCzas("Godzina", "Pora_Dnia") values (8, 'Rano');
+insert into dbo.DimCzas("Godzina", "Pora_Dnia") values (9, 'Rano');
+insert into dbo.DimCzas("Godzina", "Pora_Dnia") values (10, 'Rano');
+insert into dbo.DimCzas("Godzina", "Pora_Dnia") values (11, 'Po³udnie');
+insert into dbo.DimCzas("Godzina", "Pora_Dnia") values (12, 'Po³udnie');
+insert into dbo.DimCzas("Godzina", "Pora_Dnia") values (13, 'Po³udnie');
+insert into dbo.DimCzas("Godzina", "Pora_Dnia") values (14, 'Godziny szczytu');
+insert into dbo.DimCzas("Godzina", "Pora_Dnia") values (15, 'Godziny szczytu');
+insert into dbo.DimCzas("Godzina", "Pora_Dnia") values (16, 'Godziny szczytu');
+insert into dbo.DimCzas("Godzina", "Pora_Dnia") values (17, 'Godziny szczytu');
+insert into dbo.DimCzas("Godzina", "Pora_Dnia") values (18, 'Godziny szczytu');
+insert into dbo.DimCzas("Godzina", "Pora_Dnia") values (19, 'Wieczor');
+insert into dbo.DimCzas("Godzina", "Pora_Dnia") values (20, 'Wieczor');
+insert into dbo.DimCzas("Godzina", "Pora_Dnia") values (21, 'Wieczor');
+insert into dbo.DimCzas("Godzina", "Pora_Dnia") values (22, 'Wieczor');
+insert into dbo.DimCzas("Godzina", "Pora_Dnia") values (23, 'Wieczor');
